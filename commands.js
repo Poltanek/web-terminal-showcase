8a13127200adbb8070ecfdf2517cbc8995ece6ac
Adam Poletek Tanweer <113802807+Stravad@users.noreply.github.com>
2024-05-18 22:15:19 +0100

    Web Terminal Model 1.0.0

---
+++ commands.js
@@ -0,0 +1,63 @@
+var password = "password";
+var linkedin = "";
+var github = "";
+var email = "";
+
+whois = [
+    "<br>",
+    "Hello, I'm Adam Tanweer, a Computer Science student at the University of Winchester.",
+    
+    
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "Visitor.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    "LinkedIn:  <a href=" + linkedin,
+    "GitHub:  <a href=" + github,
+    "Email:  <a href=" + email,
+    "<br>"
+]
+
+secret = [
+    "<br>",
+    '<span class="command">sudp</span>    only use if you\'re a admin',
+    "<br>"
+]
+
+projects = [
+    "<br>",
+    "Most of the projects that I've done will be on my GitHub.",
+    "<br>"
+]
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>    Information about me',
+    '<span class="command">whoami</span>    Information about you',
+    '<span class="command">social</span>    Social media links',
+    '<span class="command">projects</span>    My projects',
+    '<span class="command">help</span>    List of commands',
+    '<span class="command">history</span>    View command history',
+    '<span class="command">email</span>    View my email address',
+    '<span class="command">clear</span>    Clear the terminal',
+    '<span class="command">banner</span>    View the "welcome" messsage',
+    '<span class="command">pwd</span>    Print working directory',
+    '<span class="command">ls</span>    Display a list of files/folders',
+    '<span class="command">cd</span>    Change directory',
+    '<span class="command">cat</span>    Display the contents of a file',
+    "<br>"
+]
+
+banner = [
+    '<h1 class="index">Welcome to Adam Tanweer\'s website | All rights reserved.</h1>',
+
+    '<span class="color2">Welcome to my portfolio with an interactive terminal!.</span>',
+    '<span class\"color2\">To get a list of available commands, type<</span> <span class=\"command\">help</span> <span class=\"color2\">and press enter.</span>',
+];
--- script.js
+++ script.js
@@ -1,35 +1,134 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const terminalOutput = document.getElementById('terminal-output');
-    const commandInput = document.getElementById('command-input');
+var before = document.getElementById('before');
+var liner = document.getElementById('liner');
+var command = document.getElementById('command');
+var textarea = document.getElementById('textarea');
+var terminal = document.getElementById('terminal');
 
-    const commands = {
-        help: 'Available commands: about\n, skills\n, projects\n, contact\n, clear\n',
-        about: 'I am a web developer with a passion for creating interactive applications.',
-        skills: 'HTML, CSS, JavaScript, React, Node.js',
-        projects: 'Project 1: A cool web app\nProject 2: Another cool project',
-        contact: 'Email: example@example.com\nLinkedIn: linkedin.com/in/example'
-    };
+var git = 0;
+var pw = false;
+var pwd = false;
+var commands = [];
 
-    const handleCommand = (input) => {
-        const command = input.trim().toLowerCase();
-        let response;
-        if (command === 'clear') {
-            terminalOutput.innerHTML = '';
-            return;
-        } else if (commands.hasOwnProperty(command)) {
-            response = commands[command];
+let currentDirectory = "";
+
+setTimeout(function() {
+    loopLines(banner, "", 80);
+    textarea.focus();
+
+}, 100);
+
+window.addEventListener('keyup', enterKey);
+
+console.log(
+    "%cYou hacked my password! You're a hacker!",
+    "color: #04ff00; font-weight: bold; font-size: 24px;"
+);
+console.log("%cPassword: '" + password + "' - Wonder what you can do with that?"),
+    "color: grey; font-weight: bold; font-size: 24px;";
+
+textarea.value = "";
+command.innerHTML = textarea.value;
+function enterKey(e) {
+    if (e.keyCode == 181) {
+        document.ATTRIBUTE_NODE.location.reload(true);
+    }
+    if (pw) {
+        let et = "*";
+        let w = textarea.value.length;
+        command.innerHTML = et.repeat(w);
+        if (textarea.value === password) {
+            pwd = true;
+        }
+        
+        if (pwd && e.keycCode == 13) {
+            loopLines(secret, "color2 margin", 120);
+            command.innerHTML = "";
+            textarea.value = "";
+            pwd = false;
+            pw = false;
+            liner.classList.remove("password");
+        } else if (e.keyCode == 13) {
+            addLine ("Wrong password", "error", 0);
+            command.innerHTML = "";
+            textarea.value = "";
+            pw = false;
+            liner.classList.remove("password");
+        }
+
+    } else {
+        if (e.keyCode == 13) {
+            commands.push(command.innerHTML);
+            git = commands.length;
+
+            if (currentDirectory === "scripts") {
+                addLine("visitor@localhost:~/" + command.innerHTML, "no-animation", 0);
+            } else {
+                addLine("visitor@localhost:~" + command.innerHTML, "no-animation", 0);
+            }
+
+            commander(command.innerHTML.toLowerCase());
+            command.innerHTML = "";
+            textarea.value = "";
+        }
+
+        if (e.keyCode == 38 && git != 0) {
+            git -= 1;
+            textarea.value = commands[git];
+            command.innerHTML = textarea.value;
+        }
+        if (e.keyCode == 40 && git < commands.length) {
+            git += 1;
+            if (commands[git] === undefined) {
+                textarea.value = "";
+            } else {
+                textarea.value = commands[git];
+            }
+            command.innerHTML = textarea.value; 
+        }
+    }
+}
+
+// Linux Commands
+
+function commander(cmd) {
+    switch(cmd.toLowerCase()) {
+        case "help":
+            loopLines(help, "color2 margin", 80);
+            break;
+
+            default:
+                addLine("<span class=\"inherit\">Command not found. For a list of commands, type <span class=\"command\">'help'</span>", "error", 100);
+    }
+}
+
+function newTab(link) {
+    setTimeout(function() {
+        window.open(link, "_blank");
+    }, 500);
+}
+
+function addLine(text, style, time) {
+    var t = "";
+    for (let i = 0; i < text.length; i++) {
+        if (text.charAt(i) === "" && text.charAt(i + 1) == "") {
+            t += "&nbsp;&nbsp;";
+            i++;
         } else {
-            response = `'${command}' is not recognized as a valid command. Type 'help' for a list of available commands.`;
+            t += text.charAt(i);
         }
-        terminalOutput.innerHTML += `<div>$ ${input}</div><div>${response}</div>`;
-        terminalOutput.scrollTop = terminalOutput.scrollHeight;
-    };
+    }
+    setTimeout(function() {
+        var next = document.createElement("p");
+        next.innerHTML = t;
+        next.className = style;
 
-    commandInput.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            const input = commandInput.value;
-            handleCommand(input);
-            commandInput.value = '';
-        }
-    });
-});
+        before.parentNode.insertBefore(next, before);
+        window.scrollTo(0, document.body.offsetHeight);
+    }, time);
+}
+
+function loopLines(array, style, time) {
+    for (let i = 0; i < array.length; i++) {
+        addLine(array[i], style, i * time);
+    }
+}
